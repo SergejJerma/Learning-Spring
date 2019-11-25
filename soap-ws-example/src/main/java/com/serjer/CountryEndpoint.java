e5ba62d67a1dcace427be2dc1bec29f7cab6574a
Serzas <j_sergej@yahoo.com>
2019-11-25 14:51:55 +0200

    Simple soap ws

---
+++ soap-ws-example/src/main/java/com/serjer/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package com.serjer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.serjer.soap_ws_example.GetCountryRequest;
+import com.serjer.soap_ws_example.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://serjer.com/soap-ws-example";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+	
+}
